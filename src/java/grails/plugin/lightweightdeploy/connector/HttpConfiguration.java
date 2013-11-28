261379d71a9e3660c4e3e4abc0f9a60e9f09ced8
Dan Everton <dan.everton@wotifgroup.com>
2013-11-28 15:36:49 +1000

    Allow almost everything to be configurable for the HTTP configuration.
    Nicks configuration and defaults from Dropwizard.

--- src/java/grails/plugin/lightweightdeploy/connector/HttpConfiguration.java
+++ src/java/grails/plugin/lightweightdeploy/connector/HttpConfiguration.java
@@ -9,6 +9,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 package grails.plugin.lightweightdeploy.connector;
 
+import com.google.common.base.Objects;
+
 import java.io.IOException;
 import java.util.Map;
 
@@ -22,19 +24,51 @@ public class HttpConfiguration {
 
     private int maxThreads = 128;
 
+    private int maxIdleTime = 200 * 1000;
+
+    private int acceptorThreads = 1;
+
+    private int acceptorThreadPriorityOffset = 0;
+
+    private int acceptQueueSize = -1;
+
+    private int maxBufferCount = 1024;
+
+    private int requestBufferSize = 16 * 1024;
+
+    private int requestHeaderBufferSize = 6 * 1024;
+
+    private int responseBufferSize = 32 * 1024;
+
+    private int responseHeaderBufferSize = 6 * 1024;
+
+    private boolean reuseAddress = true;
+
+    private Integer soLingerTime = null;
+
+    private int lowResourcesMaxIdleTime = 0;
+
     private SslConfiguration sslConfiguration;
 
     public HttpConfiguration(final Map<String, ?> httpConfig) throws IOException {
         this.port = (Integer) httpConfig.get("port");
-        if (httpConfig.containsKey("adminPort")) {
-            this.adminPort = (Integer) httpConfig.get("adminPort");
-        }
-        if (httpConfig.containsKey("minThreads")) {
-            this.minThreads = (Integer) httpConfig.get("minThreads");
-        }
-        if (httpConfig.containsKey("maxThreads")) {
-            this.maxThreads = (Integer) httpConfig.get("maxThreads");
-        }
+
+        this.adminPort = Objects.firstNonNull((Integer) httpConfig.get("adminPort"), adminPort);
+        this.minThreads = Objects.firstNonNull((Integer) httpConfig.get("minThreads"), minThreads);
+        this.maxThreads = Objects.firstNonNull((Integer) httpConfig.get("maxThreads"), maxThreads);
+        this.maxIdleTime = Objects.firstNonNull((Integer) httpConfig.get("maxIdleTime"), maxIdleTime);
+        this.acceptQueueSize = Objects.firstNonNull((Integer) httpConfig.get("acceptQueueSize"), acceptQueueSize);
+        this.acceptorThreads = Objects.firstNonNull((Integer) httpConfig.get("acceptorThreads"), acceptorThreads);
+        this.acceptorThreadPriorityOffset = Objects.firstNonNull((Integer) httpConfig.get("acceptorThreadPriorityOffset"), acceptorThreadPriorityOffset);
+        this.maxBufferCount = Objects.firstNonNull((Integer) httpConfig.get("maxBufferCount"), maxBufferCount);
+        this.requestBufferSize = Objects.firstNonNull((Integer) httpConfig.get("requestBufferSize"), requestBufferSize);
+        this.requestHeaderBufferSize = Objects.firstNonNull((Integer) httpConfig.get("requestHeaderBufferSize"), requestHeaderBufferSize);
+        this.responseBufferSize = Objects.firstNonNull((Integer) httpConfig.get("responseBufferSize"), responseBufferSize);
+        this.responseHeaderBufferSize = Objects.firstNonNull((Integer) httpConfig.get("responseHeaderBufferSize"), responseHeaderBufferSize);
+        this.reuseAddress = Objects.firstNonNull((Boolean) httpConfig.get("reuseAddress"), reuseAddress);
+        this.soLingerTime = Objects.firstNonNull((Integer) httpConfig.get("soLingerTime"), soLingerTime);
+        this.lowResourcesMaxIdleTime = Objects.firstNonNull((Integer) httpConfig.get("lowResourcesMaxIdleTime"), lowResourcesMaxIdleTime);
+
         if (httpConfig.containsKey("ssl")) {
             Map<String, ?> sslConfig = (Map<String, ?>) httpConfig.get("ssl");
             this.sslConfiguration = new SslConfiguration(sslConfig);
@@ -63,6 +97,54 @@ public class HttpConfiguration {
 
     public SslConfiguration getSslConfiguration() {
         return sslConfiguration;
+    }
+
+    public int getMaxIdleTime() {
+        return maxIdleTime;
+    }
+
+    public int getAcceptorThreads() {
+        return acceptorThreads;
+    }
+
+    public int getAcceptorThreadPriorityOffset() {
+        return acceptorThreadPriorityOffset;
+    }
+
+    public int getAcceptQueueSize() {
+        return acceptQueueSize;
+    }
+
+    public int getMaxBufferCount() {
+        return maxBufferCount;
+    }
+
+    public int getRequestBufferSize() {
+        return requestBufferSize;
+    }
+
+    public int getRequestHeaderBufferSize() {
+        return requestHeaderBufferSize;
+    }
+
+    public int getResponseBufferSize() {
+        return responseBufferSize;
+    }
+
+    public int getResponseHeaderBufferSize() {
+        return responseHeaderBufferSize;
+    }
+
+    public boolean isReuseAddress() {
+        return reuseAddress;
+    }
+
+    public Integer getSoLingerTime() {
+        return soLingerTime;
+    }
+
+    public int getLowResourcesMaxIdleTime() {
+        return lowResourcesMaxIdleTime;
     }
 
     public boolean isMixedMode() {
